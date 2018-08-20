52d1ce6c4a74fa60d9a1948f18969ff0b2a8fc2a
Michael H <hellercmichael@gmail.com>
2018-08-20 18:56:25 -0400

    Make client application post to discord.

--- feed-client/index.js
+++ feed-client/index.js
@@ -3,6 +3,8 @@ var _ = require('lodash')
 var io = require('socket.io-client')
 var socket = io.connect('http://localhost:2000/feed')
 
+var Discord = require('./../discord.gg/webhook')
+
 var game = 'pd0w5n31' // hidden_paws
 
 socket
@@ -22,4 +24,86 @@ socket
   .on('game-run', run => {
     console.log("This is the game I'm expecting (on the game feed)")
     console.log(run.weblink)
+
+    // Do something with the run.
+    process_run(run)
   })
+
+var process_run = (run) => {
+  // Get relevant information.
+  var link = run.weblink
+  var category = run.category
+  /**
+   * {
+   *  videos: {
+   *   links: [{
+   *    uri: '<video url>'
+   *   }]
+   *  }
+   * }
+   */
+  var video = _.isEmpty(run.videos) ? "" : _.head(run.videos.links).uri
+  var comment = run.comment
+  /**
+   * {
+   *  players: [{
+   *   rel: 'user',
+   *   id: '134124124',
+   *   uri: '<api uri to user>'
+   *  }]
+   * }
+   */
+  var player = _.head(run.players).id
+  /**
+   * times: {
+   *  primary: "PT22.760S",
+   *  primary_t: 22.76,
+   *  realtime: null,
+   *  realtime_t: 0,
+   *  realtime_noloads: null,
+   *  realtime_noloads_t: 0,
+   *  ingame: "PT22.760S",
+   *  ingame_t: 22.76
+   * }
+   */
+  var time = run.times.primary_t
+
+  // Turn into webhook json.
+  var title = `${player} has posted a time to ${category} leaderboard. Click here to see it.`
+  var url = link
+  var fields = { category, time, comment, video }
+
+  var embedded_fields = _.reduce(fields, (acc, value, key) => {
+    var field = {
+      name: _.startCase(key),
+      value: (value || '').toString()
+    }
+
+    if (!_.isEmpty(field.value)) {
+      acc.push(field)
+    }
+
+    return acc
+  }, [])
+
+  // Post run to discord.
+  post_run(title, url, embedded_fields)
+}
+
+var post_run = (title, url, fields) => {
+  var url = 'https://discordapp.com/api/webhooks/286673585894981632/8ZhfK1EuB5XqClMlWbH-LHGbHvMkwZJNypElumZkBw7xc4uuTniG4XbWpOmZ_Jmess4q'
+  var json = {
+    embeds: [{
+      color: 16777215,
+      title,
+      url,
+      type: 'link',
+      fields
+    }]
+  }
+
+  // Post to discord webhook.
+  Discord.post(url, json)
+    .then(response => console.log)
+    .catch(error => console.log)
+}
